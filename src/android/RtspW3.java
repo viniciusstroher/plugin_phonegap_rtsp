78a85f11953e018278090ce6e75809451385a11c
vinicius~~ <viniciusferreirwk@gmail.com>
2017-03-05 16:58:28 -0300

    a

--- src/android/RtspW3.java
+++ src/android/RtspW3.java
@@ -46,8 +46,8 @@ import org.apache.cordova.CordovaWebView;
 import org.json.JSONArray;
 import org.json.JSONException;
 
-public class SplashScreen extends CordovaPlugin {
-    private static final String LOG_TAG = "SplashScreen";
+public class RtspW3 extends CordovaPlugin {
+    private static final String LOG_TAG = "RtspW3";
     // Cordova 3.x.x has a copy of this plugin bundled with it (SplashScreenInternal.java).
     // Enable functionality only if running on 4.x.x.
     private static final boolean HAS_BUILT_IN_SPLASH_SCREEN = Integer.valueOf(CordovaWebView.CORDOVA_VERSION.split("\\.")[0]) < 4;
