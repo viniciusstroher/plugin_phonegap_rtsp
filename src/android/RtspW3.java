fdb0c4406ca83aa139ee7bd876e8db8c05f10eac
Vinicius <viniciusferreirawk@gmail.com>
2017-03-06 11:53:11 -0300

    fix

--- src/android/RtspW3.java
+++ src/android/RtspW3.java
@@ -1,22 +1,3 @@
-/*
-       Licensed to the Apache Software Foundation (ASF) under one
-       or more contributor license agreements.  See the NOTICE file
-       distributed with this work for additional information
-       regarding copyright ownership.  The ASF licenses this file
-       to you under the Apache License, Version 2.0 (the
-       "License"); you may not use this file except in compliance
-       with the License.  You may obtain a copy of the License at
-
-         http://www.apache.org/licenses/LICENSE-2.0
-
-       Unless required by applicable law or agreed to in writing,
-       software distributed under the License is distributed on an
-       "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
-       KIND, either express or implied.  See the License for the
-       specific language governing permissions and limitations
-       under the License.
-*/
-
 package org.apache.cordova.rtspw3.RtspW3;
 
 import android.app.Dialog;
@@ -49,6 +30,8 @@ import org.json.JSONException;
 public class RtspW3 extends CordovaPlugin {
     private static final String LOG_TAG = "RtspW3";
 
+    private CallbackContext callbackContext;
+    private JSONObject params;
 
     /**
      * Remember last device orientation to detect orientation changes.
@@ -112,11 +95,16 @@ public class RtspW3 extends CordovaPlugin {
         }*/
 
         if (action.equals("abrirRtsp")) {
-            /*cordova.getActivity().runOnUiThread(new Runnable() {
-                public void run() {
-                    webView.postMessage("splashscreen", "hide");
-                }
-            });*/
+            //pega parametros do js
+            this.params = args.getJSONObject(0);
+            Intent intent = new Intent(cordova.getActivity(), RtspW3Activity.class);
+
+            //LINK PARA ENVIAR PARA A ACTIVITY
+            intent.putExtra("LINK_RTSP", this.params.getString("link"));
+
+            if (this.cordova != null) {
+                this.cordova.startActivityForResult((CordovaPlugin) this, intent, 0);
+            }
         }
 
         callbackContext.success();
