7f2a927f15f043a7ec340905ad0b30b6c34cd527
Vinicius <viniciusferreirawk@gmail.com>
2017-03-06 12:02:21 -0300

    fix

---
+++ src/android/RtspW3Activity.java
@@ -0,0 +1,42 @@
+package org.apache.cordova.rtspw3.RtspW3;
+
+import android.app.Dialog;
+import android.app.ProgressDialog;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.res.Configuration;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
+import android.os.Handler;
+import android.view.Display;
+import android.view.Gravity;
+import android.view.View;
+import android.view.ViewGroup.LayoutParams;
+import android.view.WindowManager;
+import android.view.animation.Animation;
+import android.view.animation.AlphaAnimation;
+import android.view.animation.DecelerateInterpolator;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.ProgressBar;
+import android.widget.RelativeLayout;
+
+import org.apache.cordova.CallbackContext;
+import org.apache.cordova.CordovaPlugin;
+import org.apache.cordova.CordovaWebView;
+import org.json.JSONArray;
+import org.json.JSONException;
+
+public class RtspW3Activity extends Activity{
+    private String link_rtsp;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        
+        String package_name = getApplication().getPackageName();
+        setContentView(getApplication().getResources().getIdentifier("rtsp_w3_acitivity", "layout", package_name));
+        
+    }
+
+}
