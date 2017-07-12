050473c6ae30417405007bd7bb4d1db4e3b4d871
vinicius~~ <viniciusferreirwk@gmail.com>
2017-07-11 21:41:14 -0300

    fix

--- src/android/RtspW3Activity.java
+++ src/android/RtspW3Activity.java
@@ -193,9 +193,8 @@ public class RtspW3Activity extends Activity{
             //options.add("--aout=opensles");
             //options.add("--audio-time-stretch"); // time stretching
             //options.add("-vvv"); // verbosity
-
-            libvlc = new LibVLC(getApplicationContext(),options);
             Log.i("RTSP","OPTIONS VLC LIB INSTANCE: "+options.toString());
+            libvlc = new LibVLC(getApplicationContext(),options);
             //libvlc = new LibVLC(getApplicationContext());
             
             holder.setKeepScreenOn(true);
